34d61a5959da1b275d8a0f80ae4d0163cf41ef11
Evan Felix <evan.felix@pnnl.gov>
2017-07-26 13:29:16 -0700

    add header for file for later additions

---
+++ .license.header.rb
@@ -0,0 +1,5 @@
+# Copyright (C) 2017 Battelle Memorial Institute
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD-2 license.  See the LICENSE file for details.
